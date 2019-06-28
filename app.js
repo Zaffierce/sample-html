54772fabc972b122e8f45b8db3f8d1360c7b4ef9
Jon Veach <veach09@gmail.com>
2019-06-27 21:03:07 -0700

    Updated app.js and hid picture in index.html

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+//sets function askQuestion to a universal function where I can pass in any prompt I want.
+//askQuestion("Hello!")
+function askQuestion(msg) {
+  answer = prompt(msg);
+}
+
+askQuestion("What is the secret password?");
+
+//var secretKey = ['Z', 'A', 'Z']
+//Only works on the index[0]....
+var secretKey = 'Brian';
+//if validated is false, don't prompt the user with customization code later.
+var validated = false;
+
+//Loops for 5 times
+for (var i = 0; i <= 5; i++) {
+  if (answer === secretKey) {
+    validated = true;
+    break;
+  } else {
+    //Asks the question 4 times.
+    if (i <= 4) {
+      //Subtracts 5 from i
+      timesRemaining = '5' - i;
+      askQuestion('ERROR:  Incorrect password, please try again.  Attempts remaining: ' + timesRemaining);
+    }
+    //If it's on the 5th time, send an alert that they've failed.
+    if (i === 5) {
+      alert("You have failed authentication too many times!");
+      //document.getElementsByTagName('body').style.display = 'none';
+      document.getElementsByName('body').style.display = 'none';
+      //element.style.display = 'none';
+    }
+  }
+}
 
 function askName() {
   setName = prompt("Please enter your name!");
@@ -13,18 +48,19 @@ function askAnimal() {
   setAnimal = prompt("Please enter an animal type.  You may choose from cat, dog or sheep.")
 }
 
+if (validated === true) {
+  alert("Successfully logged in.  Please fill in the following data...")
+
 askName();
 askColor();
 askAnimal();
+//sets askAnimal to lowercase
 setAnimalLC = setAnimal.toLowerCase()
 
 function giveAnswer() {
   document.write(fact)
   document.write(picture)
 }
-
-//want to do:  create an array with cat, dog, sheep and validate response against that.
-//var animalArray = ['cat','dog','sheep'];
 
 if (setAnimalLC === "cat" || setAnimalLC === "dog" || setAnimalLC === "sheep") {
   if (setAnimalLC === "cat") {
@@ -45,3 +81,74 @@ if (setAnimalLC === "cat" || setAnimalLC === "dog" || setAnimalLC === "sheep") {
 } else {
   alert("Boo you suck.  You failed to enter a valid response, so now you won't get the super cool fact about that animal!")
 }
+} 
+
+
+
+
+
+
+
+// while (end < 10) {
+//   if (answer === "Jon") {
+//     alert("Yay!")
+//     break;
+//   } else {
+//     askQuestion();
+//     end++;
+//   }
+// }
+// answerLC = answer.toLowerCase();
+
+// for(i = 0; i < response.length; i++) {
+//   if (answerLC === "Jon") {
+//     alert("True")
+//   } else {
+//     alert("false")
+//   }
+// }
+
+
+// if (answer[0] === "3") {
+//   alert("True")
+// } else {
+//   alert("false")
+// }
+
+
+// for(var i = 0; i < response.length; i++) {
+//   if (!correctResponse) { 
+//     alert("Incorrect")
+//   } else { alert("Correct")}
+// }
+
+// if (nums[0] === "3") {
+//   document.write("You've got it!  My favorite number is 3")
+// } else {
+//   answer = prompt("What is my favorite number?");
+// }
+
+// while (nums[0] !== '3') {
+//   answer = prompt("Please try again!")
+//   } 
+
+
+
+//document.write(nums);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
